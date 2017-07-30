434900bbb7a983b832bf3bd62c24dd1e4757a52f
Natalia Romanyuk <romanyukn@gmail.com>
2017-07-30 15:08:38 +0700

    Doubles become string

--- Giraffe/ViewController.swift
+++ Giraffe/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    
     
     @IBOutlet weak var text2: UITextField!
     @IBOutlet weak var text1: UITextField!
@@ -20,11 +20,7 @@ class ViewController: UIViewController {
     
     @IBAction func battonTapped(_ sender: AnyObject) {
         
-        tapCount = tapCount + 1
-        if tapCount >= 10 {
-            theLabel.text = "You tapped it 10 times"
-        }
-        
+        theLabel.text = "Answer is: \(Double(text1.text!)! + Double(text2.text!)!)"
         
     }
     
